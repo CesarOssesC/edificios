5c30f58ba53d3b083279bd4a713122bedaf73483
César Osses <cesar.osses@gmail.com>
2022-08-03 11:52:17 -0400

    ejercicio edificios

---
+++ app/controllers/edificios_controller.rb
@@ -0,0 +1,52 @@
+class EdificiosController < ApplicationController
+    before_action :set_edificio, only: %i[ show edit update destroy ]
+    
+    def index
+        @edificios = Edificio.all.order(created_at: :desc)
+    end
+
+    def show
+        
+    end
+
+    def new
+        @edificio = Edificio.new
+    end
+
+    def edit
+
+    end
+
+    def create
+        @edificio = Edificio.new(edificio_params)
+        if @edificio.save
+            redirect_to edificios_path, notice: "Edificio Creado Exitosamente."
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @edificio.update(edificio_params)
+            redirect_to @edificio, notice: "Edificio actualizado Exitosamente."
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @edificio.destroy
+        redirect_to edificios_url, notice: "Edificio Eliminado Exitosamente." 
+    end
+
+
+    private
+
+    def set_edificio
+        @edificio = Edificio.find(params[:id])
+    end
+
+    def edificio_params
+        params.require(:edificio).permit(:nombre, :direccion, :ciudad)
+    end
+end
---
+++ app/models/edificio.rb
@@ -0,0 +1,8 @@
+class Edificio < ApplicationRecord
+    has_many :departamento, dependent: :delete_all
+
+    validates :nombre, presence: true
+    validates :direccion, presence: true
+    validates :ciudad, presence: true
+    
+end
